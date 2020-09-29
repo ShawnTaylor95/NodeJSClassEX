b1e5703d064e4965c0413ae4330e4593002a72a1
ShawnTaylor95 <Taylor_Shawn2@Columbusstate.edu>
2020-09-29 18:14:09 +0000

    added posts

--- index.js
+++ index.js
@@ -1,19 +1,51 @@
 //require express
 var express = require('express');
+//require body-parser
+var bodyParser= require('body-parser');
 
 //create express object, call express
 var app = express();
 
 //tell application to use ejs for templates
-apps.set('view engine', 'ejs');
+app.set('view engine', 'ejs');
+// tell app to use body-parser
+app.use(bodyParser.urlencoded({extended: true}));
+
+//couple of items
+var tasks = ["make it to class", "take child to daycare"];
+
+//completed items
+var completed = ["extra work"];
+
 
 // get homepage
 app.get('/', function(req,res){
 //return something to homepage
-    res.send('Hello World!');
-
+    res.render('index', {tasks: tasks}); //add completed variable to ejs ex {a:a,b:b}
 });
 
+//add post / addtask
+app.post('/addtask', function(req,res){
+    //create new variable
+    var newTask = req.body.newtask;
+    tasks.push(newTask);
+    //redirects code to app.get so the code won't be redundant.. still pushes new task code to list on homepage
+    res.redirect('/');
+});
+
+//remove tasks
+app.post('/removetask', function(req,res){
+    //push to completed
+    var removeTask = req.body.check;
+    if(typeof removeTask === 'string'){
+        tasks.splice(tasks.indexOf(removeTask),1);
+    }else if(typeof removeTask === 'object'){
+        for(var i = 0; i< removeTask.length; i++){
+            tasks.splice(tasks.indexOf(removeTask[i]),1);
+        }
+    }
+    res.redirect('/');
+});
 
 //server setup
 app.listen(3000,function(){
